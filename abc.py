59aa6dc4b4087094a3e84889b90b9c719a359260
Ratan-Priyanshu <120351893+Ratan-Priyanshu@users.noreply.github.com>
2024-12-06 13:23:56 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,14 @@
+# task 125102
+
+def parcel():
+    return
+def fuse():
+    return
+def _100copper():
+    return
+def _30arena():
+    return
+def _1BPtask():
+    return
+def _5Wood():
+    return
